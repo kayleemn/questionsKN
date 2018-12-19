667c0a6eacf5094eca90e0e32da62483da1b0cd6
csPython <cspython@AD25TF04NGF1J-110.local>
2018-12-19 12:44:30 -0500

    finish adding the test

--- questionsKN01.py
+++ questionsKN01.py
@@ -17,6 +17,15 @@ while check1 == False:
     try:
         a1 = int(input("Choose the best reponse based on your knowledge on Drake"))
         if a1 == 2:
-            print("Okay Cool!")
-            co
+            print("Okay Cool!") #acceptable
+            score = int(score+1)
+            check1 = True
+        elif 0 < a1 < 5: #acceptable
+            print("Got it!")
+            check1 = True
+        else:
+            print("Please enter an integer between 0-5") #non-acceptable
+    except ValueError:
+        print("Um.. let's think a little harder")
+        
         
